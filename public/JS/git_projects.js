5726c74a32d01d2469045533da798a4940958701
Ethan Heavey <ethanheavey@outlook.com>
2023-01-10 12:41:11 -0400

    Start tool column process

--- public/JS/git_projects.js
+++ public/JS/git_projects.js
@@ -6,6 +6,7 @@ async function getRepos() {
 
 	for (let i = 0; i < data.length; i++) {
 		const repo = data[i];
+		console.log(repo);
 		if (repo.fork == false && repo.name != 'game-development' && repo.name != 'python-case-studies') {
 			const row = document.createElement('tr');
 			const nameCell = document.createElement('td');
@@ -23,23 +24,24 @@ async function getRepos() {
 				window.open(repo.html_url);
 			});
 			non_fork_table.appendChild(row);
-		} else {
-			const row = document.createElement('tr');
-			const nameCell = document.createElement('td');
-			nameCell.textContent = repo.name;
-			const descCell = document.createElement('td');
-			descCell.textContent = repo.description;
-			const urlRedirect = document.createElement('a');
-			// urlRedirect.textContent = "Take me to the repo"
-			urlRedirect.href = repo.html_url;
-			urlRedirect.target = '_blank';
-			nameCell.appendChild(urlRedirect);
-			descCell.appendChild(urlRedirect);
-			row.appendChild(nameCell);
-			row.appendChild(descCell);
-			// row.appendChild(urlRedirect);
-			fork_table.appendChild(row);
 		}
+		// else {
+		// 	const row = document.createElement('tr');
+		// 	const nameCell = document.createElement('td');
+		// 	nameCell.textContent = repo.name;
+		// 	const descCell = document.createElement('td');
+		// 	descCell.textContent = repo.description;
+		// 	const urlRedirect = document.createElement('a');
+		// 	// urlRedirect.textContent = "Take me to the repo"
+		// 	urlRedirect.href = repo.html_url;
+		// 	urlRedirect.target = '_blank';
+		// 	nameCell.appendChild(urlRedirect);
+		// 	descCell.appendChild(urlRedirect);
+		// 	row.appendChild(nameCell);
+		// 	row.appendChild(descCell);
+		// 	// row.appendChild(urlRedirect);
+		// 	fork_table.appendChild(row);
+		// }
 	}
 }
 
